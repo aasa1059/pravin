98d8687162364072d2dc15fd5de51924078436e9
aasa1059 <pravinaasa7960@gmail.com>
2017-06-12 18:16:26 +0530

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner d=new Scanner(System.in);
+		int n=d.nextInt();
+	    int a=(int) Math.log10(n)+1;
+		System.out.println(a);
+	}
+}
