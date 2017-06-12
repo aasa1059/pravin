e1ef09111f97e719ef3f7028dd075502e9b9ab20
aasa1059 <pravinaasa7960@gmail.com>
2017-06-12 10:06:19 +0530

    Create num.java

--- num.java
+++ num.java
@@ -1,1 +1,20 @@
-# pravin
+import java.io.*;
+import java.util.*;
+class num
+{
+  public static void main(String args[]);
+  Scanner sc-new Scanner(System.in)
+    int n=sc.nextInt();
+  if(n==0)
+  {
+    System.out.println("zero");
+  }
+  else if(n>0)
+  {
+    System.out.println("negative");
+  }
+  else
+  {
+    System.out.println("positive");
+  }
+}
