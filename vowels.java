4abb596d443572912127a6550d976a05233d2fab
aasa1059 <pravinaasa7960@gmail.com>
2017-06-12 18:34:34 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    char n=sc.nextLine().charAt(0);
+	    switch(n)
+
+        {
+
+            case 'a':
+
+            System.out.println("vowel");
+
+            break;
+
+ 
+
+            case 'e':
+
+            System.out.println("vowel");
+
+            break;
+
+ 
+
+            case 'i':
+
+            System.out.println("vowel");
+
+            break;
+
+ 
+
+            case 'o':
+
+            System.out.println("vowel");
+
+            break;
+
+ 
+
+            case 'u':
+
+            System.out.println("vowel");
+
+            break;
+
+ 
+
+            default:
+
+            System.out.println("consonant");
+
+            break;
+
+        }
+	}
+}
