9bcf69f72bf19fe128c3046fd9580a563904aca4
aasa1059 <pravinaasa7960@gmail.com>
2017-06-12 14:52:41 +0530

    Create largestnum.java

---
+++ largestnum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner g=new Scanner(System.in);
+		int n1=g.nextInt();
+		int n2=g.nextInt();
+		if(n1>n2)
+		{
+		    System.out.println(+n1);
+		}
+		else if(n1<n2)
+		{
+		    System.out.println(+n2);
+		}
+	}
+}
